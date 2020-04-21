98c3da25f45aeb0e962c1157ee3b8c08d432abf0
Benzhouid <63436371+Benzhouid@users.noreply.github.com>
2020-04-21 17:18:30 +0800

    add log files

---
+++ StartProxyClient.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+SVR_HOST=59.111.176.145
+SVR_PORT=5438
+KEY=YourKey
+LOG_PATH=/root/NetworkProxy
+JAR_PATH=/root/NetworkProxy
+PRO_NAME=com.netease.mail.qiye.StartProxyApplication
+
+for jar in ${JAR_PATH}/*.jar
+do
+CLASSPATH=$jar:$CLASSPATH
+done
+
+export CLASSPATH
+export LANG="zh_CN.GBK"
+
+while true ; do
+    NUM=`ps -ef | grep ${PRO_NAME} | grep ${KEY} | grep -v grep| wc -l`
+    if [ "${NUM}" -lt "9" ];then
+    	nohup java ${PRO_NAME} ${SVR_HOST} ${SVR_PORT} ${KEY} >> ${LOG_PATH}/nohup-proxy-client.log 2>&1 &
+    fi
+    sleep 5
+done
+exit 0
+
